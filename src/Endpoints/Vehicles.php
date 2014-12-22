b1cd24906a86a5a0f8e6d88b1534fdf13d5d9ac9
Ross Masters <ross@rossmasters.com>
2014-12-22 01:11:25 +0000

    Fix urls, add Vehicle::index()

--- src/Endpoints/Vehicles.php
+++ src/Endpoints/Vehicles.php
@@ -2,13 +2,26 @@
 
 namespace SWAPI\Endpoints;
 
+use ArrayObject;
 use SWAPI\Models\Vehicle;
 
 class Vehicles extends Endpoint
 {
+    public function index($page = null)
+    {
+        $request = $this->http->createRequest("GET", "vehicles/");
+        $response = $this->http->send($request);
+
+        if ($response->getStatusCode() == 200) {
+            return $this->mapper->mapArray($response->json()['results'], new ArrayObject, 'SWAPI\Models\Vehicle');
+        }
+
+        return $this->handleResponse($response, $request);
+    }
+
     public function get($id)
     {
-        $request = $this->http->createRequest("GET", sprintf("/vehicles/%d", $id));
+        $request = $this->http->createRequest("GET", sprintf("vehicles/%d/", $id));
         $response = $this->http->send($request);
 
         if ($response->getStatusCode() == 200) {
